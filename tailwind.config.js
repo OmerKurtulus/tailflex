de3bda198e9e8b406eac26feb863322b51638794
OmerKurtulus <omerfarukkrtls@gmail.com>
2024-08-30 21:36:16 +0300

    Tailwind CSS Font And Color

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+    },
+    extend: {
+      fontFamily:{
+        poppins : ['Poppins', 'system-ui'],
+        josefin : ['Josefin Sans', 'system-ui']
+      },
+      colors:{
+        'tailflix-red' : '#BC1A45',
+        'tailflix-melon' : '#FFD369',
+        'tailflix-grey' : '#DDDDDD',
+        'tailflix-white' : '#F7F7F7'
+      },
+    },
   },
   plugins: [],
 }
